2fcf97696a4746a88799bb613475fdc5af7be5ab
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-05-28 17:22:48 -0700

    Rename handleNumberClick to handleClickNumber

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -8,7 +8,7 @@ class Calculator extends Component {
   constructor() {
     super();
 
-    this.handleNumberClick = this.handleNumberClick.bind(this);
+    this.handleClickNumber = this.handleClickNumber.bind(this);
     this.handleClickAllClear = this.handleClickAllClear.bind(this);
     this.handleClickClearEntry = this.handleClickClearEntry.bind(this);
     this.handleClickSum = this.handleClickSum.bind(this);
@@ -18,7 +18,7 @@ class Calculator extends Component {
     };
   }
 
-  handleNumberClick(number) {
+  handleClickNumber(number) {
     this.setState(clickNumber.bind(null, number));
   }
 
@@ -41,27 +41,27 @@ class Calculator extends Component {
           <CalculatorScreen value={this.state.entry} />
           <Button value="AC" onClick={this.handleClickAllClear} />
           <Button value="CE" onClick={this.handleClickClearEntry} />
-          <Button value="%" onClick={this.handleNumberClick} />
-          <Button value="/" onClick={this.handleNumberClick} />
-          <Button value="7" onClick={this.handleNumberClick} />
-          <Button value="8" onClick={this.handleNumberClick} />
-          <Button value="9" onClick={this.handleNumberClick} />
-          <Button value="x" onClick={this.handleNumberClick} />
-          <Button value="4" onClick={this.handleNumberClick} />
-          <Button value="5" onClick={this.handleNumberClick} />
-          <Button value="6" onClick={this.handleNumberClick} />
-          <Button value="-" onClick={this.handleNumberClick} />
-          <Button value="1" onClick={this.handleNumberClick} />
-          <Button value="2" onClick={this.handleNumberClick} />
-          <Button value="3" onClick={this.handleNumberClick} />
+          <Button value="%" onClick={this.handleClickNumber} />
+          <Button value="/" onClick={this.handleClickNumber} />
+          <Button value="7" onClick={this.handleClickNumber} />
+          <Button value="8" onClick={this.handleClickNumber} />
+          <Button value="9" onClick={this.handleClickNumber} />
+          <Button value="x" onClick={this.handleClickNumber} />
+          <Button value="4" onClick={this.handleClickNumber} />
+          <Button value="5" onClick={this.handleClickNumber} />
+          <Button value="6" onClick={this.handleClickNumber} />
+          <Button value="-" onClick={this.handleClickNumber} />
+          <Button value="1" onClick={this.handleClickNumber} />
+          <Button value="2" onClick={this.handleClickNumber} />
+          <Button value="3" onClick={this.handleClickNumber} />
           <Button
             className="Button-big"
             value="+"
             onClick={this.handleClickSum}
           />
-          <Button value="0" onClick={this.handleNumberClick} />
-          <Button value="." onClick={this.handleNumberClick} />
-          <Button value="=" onClick={this.handleNumberClick} />
+          <Button value="0" onClick={this.handleClickNumber} />
+          <Button value="." onClick={this.handleClickNumber} />
+          <Button value="=" onClick={this.handleClickNumber} />
         </div>
       </div>
     );
